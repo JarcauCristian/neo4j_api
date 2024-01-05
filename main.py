4a91fb644cdcd5292373a0f1962b364b91291ef9
JarcauCristian <stefan.jarcau@student.unitbv.ro>
2024-01-05 14:36:41 +0200

    Polished some endpoints

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from starlette.middleware.cors import CORSMiddleware
+
 from neo4j_driver.driver import Neo4jDriver
 from neo4j import GraphDatabase
 from fastapi import FastAPI
@@ -8,6 +10,20 @@ from dotenv import load_dotenv
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "https://localhost:3000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def entry():
@@ -17,19 +33,60 @@ async def entry():
 @app.get("/all")
 async def get_all():
     query = """
-        MATCH (n)-[r]->(m)
-        RETURN n, r, m
-        """
+            MATCH (n)-[r]->(m)
+            OPTIONAL MATCH (n)<-[r2]-(upperNode)
+            WITH n, COLLECT(DISTINCT m) AS upper, COLLECT(DISTINCT upperNode) AS under_nodes
+            RETURN n.name AS node_name, 
+                   CASE WHEN SIZE(upper) > 0 THEN upper[0].name ELSE NULL END AS upper_node,
+                   CASE WHEN n.labels <> "Category" THEN "has_info" ELSE NULL END AS has_info,
+                   under_nodes
+            """
     result = driver.query(query)
-    return JSONResponse(status_code=200, content=result)
+
+    formatted_result = [
+        {
+            "upper_node": None,
+            "under_nodes": [],
+            "name": "Base",
+            "hasInformation": False
+        }
+    ]
+    for record in result:
+        node_name = record["node_name"]
+        upper_node = record["upper_node"]
+        under_nodes = record["under_nodes"]
+        has = record["has_info"]
+
+        if upper_node == "Base":
+            for index, node in enumerate(formatted_result):
+                if node["name"] == "Base":
+                    formatted_result[index]["under_nodes"].append(node_name)
+                    break
+
+        formatted_result.append({
+            "name": node_name,
+            "upper_node": upper_node,
+            "under_nodes": under_nodes,
+            "hasInformation": True if has == "has_info" else False
+        })
+
+    return JSONResponse(status_code=200, content=formatted_result)
 
 
 @app.post("/category/create")
 async def create_category(name: str):
     query = (
-        "CREATE (n:Person {name: $name})"
+        "MERGE(m:MainNode {name: 'Base'})"
+        "MERGE (n:Category {name: $name})-[:BELONGS_TO]->(m)"
         "RETURN id(n) AS node_id, n.name AS node_name"
     )
+
+    try:
+        driver.query(query, parameters={"name": name}, fetch_one=True)
+    except:
+        return JSONResponse(status_code=500, content="An error occurred when creating the category!")
+
+    return JSONResponse(status_code=200, content="Category created successfully!")
 
 
 if __name__ == "__main__":
